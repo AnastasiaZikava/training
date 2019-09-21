4c285423218f78d645830246cf0bd9953f36f95a
AnastasiaZikova <nzykava@aristeksystems.com>
2019-09-22 02:03:51 +0300

    Lesson 9: OOP, Part 1

---
+++ lesson_9.rb
@@ -0,0 +1,40 @@
+class Message 
+  @@messages_sent = 0
+  def initialize(from, to)
+    @from = from 
+    @to = to 
+    @@messages_sent +=1 
+  end
+end
+
+class Email < Message
+  def initialize(from, to)
+    super
+  end
+end
+
+my_message = Message.new("Ian", "Alex")
+
+#############################
+
+class Computer
+  @@users = {}
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @files = {}
+    @@users[username] = password
+  end
+  
+  def create(filename)
+    time = Time.now
+    @files[filename] = time
+    puts "#{filename} was created at #{time} by #{@username}. "
+  end
+  
+  def Computer.get_users
+    @@users
+  end
+end
+
+my_computer = Computer.new('nastya', 12345)
