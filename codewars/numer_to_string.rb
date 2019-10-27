f273179ddf323b4114b282c3d0b425c72ef53b06
AnastasiaZikova <nzykava@aristeksystems.com>
2019-10-27 23:59:29 +0300

    add string tasks

---
+++ codewars/numer_to_string.rb
@@ -0,0 +1,11 @@
+=begin
+Write a function which removes from string all non-digit characters and parse
+the remaining to number. E.g: "hell5o wor6ld" -> 56
+Function:
+get_number_from_string(s)
+=end
+
+
+def get_number_from_string(s)
+  s.gsub(/[^\d]/,"").to_i
+end
